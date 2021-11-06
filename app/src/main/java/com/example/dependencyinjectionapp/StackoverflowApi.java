0f28ff1fa595ed96b279b6306339e23c82ef60e1
Rameeshatariq <meeshatariq@gmail.com>
2021-11-06 07:12:27 -0700

    [Added]

---
+++ app/src/main/java/com/example/dependencyinjectionapp/StackoverflowApi.java
@@ -0,0 +1,15 @@
+package com.example.dependencyinjectionapp;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface StackoverflowApi {
+
+    @GET("/questions?order=desc&sort=activity&site=stackoverflow")
+    Call<QuestionsListResponseSchema> lastActiveQuestions(
+            @Query("pagesize") Integer pageSize
+    );
+
+
+}
